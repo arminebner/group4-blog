eda752244270868744f5f72ad35f81c87894d22b
Armin Ebner <armin_ebner@web.de>
2020-12-08 17:19:20 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      {/* Navbar 
+            Router
+              Homepage
+              All-Recipes
+              Single-Recipe
+            Router
+          Footer
+      */}
+     
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/All.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const All = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default All
