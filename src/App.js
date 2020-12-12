07226dca986db927df2adf9d55ee86f9e58c5f4f
Armin Ebner <armin_ebner@web.de>
2020-12-12 13:17:41 +0100

    have to commit sth

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import './App.css';
+import './armin.css'
+
 
 function App() {
   return (
@@ -11,6 +13,16 @@ function App() {
             Router
           Footer
       */}
+       <div className="lightbox">
+                <div className="letter-container">
+                    <div className="letter">C</div>
+                    <div className="letter">I</div>
+                    <div className="letter">N</div>
+                    <div className="letter">E</div>
+                    <div className="letter">M</div>
+                    <div className="letter">A</div>
+                </div>
+            </div>
      
     </div>
   );
